71c7bce93f7a8a9aadcde78772f666d93859ce0e
yangdanping <1240645840@qq.com>
2023-03-02 16:47:05 +0800

    bug fix

--- src/router/main/story/chat/chat.ts
+++ src/router/main/story/chat/chat.ts
@@ -1,4 +1,4 @@
-const Chat = () => import('@/views/main/story/chat/Chat.vue');
+const Chat = () => import('@/views/main/story/chat/chat.vue');
 export default {
   path: '/main/story/chat',
   name: 'chat',
